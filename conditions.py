1a86f5dc7c7c0ba948d28b7de3a21b1dfd30b6e3
bradybarry <86431211+bradybarry@users.noreply.github.com>
2021-07-07 20:31:08 -0400

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,14 @@
+temp = int(int("Enter the tempature outside: "))
+if temp in range (90,104):
+    print("Wear Shorts")
+elif temp in range (70,89):
+    print("Shoer Sleeves are fine")
+elif temp in range (50,69):
+    print("wear a Jacket")
+elif temp in range (32:49):
+     print("Wear a heavy coat")
+else:
+    print ("Stay Inside")
+
+    
+    
